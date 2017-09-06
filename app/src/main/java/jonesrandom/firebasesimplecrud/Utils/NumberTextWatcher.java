fc6fd1f8aba8ddbc8ed1398fe7ca76199a4c30fa
JonesRandom <jones.random97@gmail.com>
2017-09-06 18:42:38 +0800

    Number Curency

---
+++ app/src/main/java/jonesrandom/firebasesimplecrud/Utils/NumberTextWatcher.java
@@ -0,0 +1,74 @@
+package jonesrandom.firebasesimplecrud.Utils;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.text.DecimalFormat;
+import java.text.ParseException;
+
+/**
+ * Created by JonesRandom on 9/6/17.
+ * https://masx-dev.blogspot.com
+ */
+
+public class NumberTextWatcher implements TextWatcher {
+
+    private DecimalFormat df;
+    private DecimalFormat dfnd;
+    private boolean hasFractionalPart;
+
+    private EditText et;
+
+    public NumberTextWatcher(EditText et) {
+        df = new DecimalFormat("#,###.##");
+        df.setDecimalSeparatorAlwaysShown(true);
+        dfnd = new DecimalFormat("#,###");
+        this.et = et;
+        hasFractionalPart = false;
+    }
+
+    @SuppressWarnings("unused")
+    private static final String TAG = "NumberTextWatcher";
+
+    public void afterTextChanged(Editable s) {
+        et.removeTextChangedListener(this);
+
+        try {
+            int inilen, endlen;
+            inilen = et.getText().length();
+
+            String v = s.toString().replace(String.valueOf(df.getDecimalFormatSymbols().getGroupingSeparator()), "");
+            Number n = df.parse(v);
+            int cp = et.getSelectionStart();
+            if (hasFractionalPart) {
+                et.setText(df.format(n));
+            } else {
+                et.setText(dfnd.format(n));
+            }
+            endlen = et.getText().length();
+            int sel = (cp + (endlen - inilen));
+            if (sel > 0 && sel <= et.getText().length()) {
+                et.setSelection(sel);
+            } else {
+                // place cursor at the end?
+                et.setSelection(et.getText().length() - 1);
+            }
+        } catch (NumberFormatException | ParseException nfe) {
+            // do nothing?
+        }
+
+        et.addTextChangedListener(this);
+    }
+
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (s.toString().contains(String.valueOf(df.getDecimalFormatSymbols().getDecimalSeparator()))) {
+            hasFractionalPart = true;
+        } else {
+            hasFractionalPart = false;
+        }
+    }
+}
